0a5c68308f667b09a42e5988385c721a97ee3faf
Tom Rohr <trohr92@live.com>
2023-03-10 18:26:00 -0800

    created components for bookmarks, todos and notes
    Creating now just so I can set routes on the menu tabs

--- Angular-Personal_Dashboard/src/app/app.module.ts
+++ Angular-Personal_Dashboard/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    BookmarksComponent,
+    NotesComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
