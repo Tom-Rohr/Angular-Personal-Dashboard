f9216c9310b80070a6391035a2fe5582570a89b3
Tom Rohr <trohr92@live.com>
2023-03-26 09:32:09 -0700

    Created bookmark model and services

---
+++ Angular-Personal_Dashboard/src/app/shared/bookmark.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Bookmark } from './bookmark.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookmarkService {
+
+  bookmarks!: Bookmark[]
+
+  constructor() { }
+
+  getBookmarks() {
+    return this.bookmarks;
+  }
+
+  getBookmark(id: string) {
+    return this.bookmarks.find(bookmark => bookmark.id === id);
+  }
+
+  addBookmark(bookmark: Bookmark) {
+    this.bookmarks.push(bookmark);
+  }
+
+  updateBookmark(id: string, updatedFields: Partial<Bookmark>) { 
+    const bookmark = this.getBookmark(id);
+    Object.assign(bookmark!, updatedFields);
+  }
+
+  deleteBookmark(id: string) {
+    const bookmarkIndex = this.bookmarks.findIndex(bookmark => bookmark.id === id);
+    if (bookmarkIndex == -1) return;
+    this.bookmarks.splice(bookmarkIndex, 1)
+  }
+}
