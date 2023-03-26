4ad3c789e71948bf7d0e757c8ea23fcfdf6bfac8
Tom Rohr <trohr92@live.com>
2023-03-25 21:22:42 -0700

    Implemented edit-todo component

--- Angular-Personal_Dashboard/src/app/edit-todo/edit-todo.component.ts
+++ Angular-Personal_Dashboard/src/app/edit-todo/edit-todo.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { TodoService } from '../shared/todo.service';
+import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-edit-todo',
   templateUrl: './edit-todo.component.html',
   styleUrls: ['./edit-todo.component.scss']
 })
-export class EditTodoComponent {
+export class EditTodoComponent implements OnInit{
 
+  todo?: Todo
+  
+  constructor(private route:ActivatedRoute, private todoService: TodoService, private router: Router) { }
+  
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId = paramMap.get('id')
+      this.todo = this.todoService.getTodo(todoId!)
+    })
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return alert("The todo cannot be empty.")
+
+    this.todoService.updateTodo(this.todo!.id, form.value)
+    this.router.navigateByUrl("/todos")
+  }
 }
